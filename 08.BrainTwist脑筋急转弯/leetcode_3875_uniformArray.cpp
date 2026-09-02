6578833047353055c0de6a1f331878bf9ca58e6f
gauss_p <gaussp23@gmail.com>
2026-09-02 13:07:49 +0800

    3875.构造奇偶一致的数组I/脑筋急转弯

---
+++ 08.BrainTwist脑筋急转弯/leetcode_3875_uniformArray.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> nums = {2,3};
+    cout << s.uniformArray(nums) << endl;
+}
---
+++ 08.BrainTwist脑筋急转弯/leetcode_3875_uniformArray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
+
+if __name__ == "__main__":
+    s = Solution()
+    nums1 = [2,3]
+    print(s.uniformArray(nums1))
